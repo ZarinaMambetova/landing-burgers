21671699714cb8dab3ac18a51ec84d521c7a1c6b
ZarinaMambetova <zarina.mambetova84@gmail.com>
2017-11-12 17:20:27 +0400

    1

--- js/main.js
+++ js/main.js
@@ -1,23 +1,11 @@
 var hamburger = document.querySelector(".hamburger");
-console.log(hamburger);
+var navigation = document.querySelector(".navigation-list")
 hamburger.addEventListener("click", function () {
     if (hamburger.classList.contains("hamburger_active")) {
         hamburger.classList.remove("hamburger_active");
-
+        navigation.classList.remove("navigation-list_active");
     } else {
         hamburger.classList.add("hamburger_active");
+        navigation.classList.add("navigation-list_active");
     }
-
-});
-
-var navigation = document.querySelector(".navigation-list");
-console.log(navigation);
-hamburger.addEventListener("click", function () {
-    if (hamburger.classList.contains("navigation-list_active")) {
-        hamburger.classList.remove("navigation-list_active");
-
-    } else {
-        hamburger.classList.add("navigation-list_active");
-    }
-
 });
